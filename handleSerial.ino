1de8b4e90b8b057ae1d283325c2504f771183a9d
DenAutonomePirat <DenAutonomePirat@users.noreply.github.com>
2017-09-11 20:55:56 +0200

    first upload

---
+++ handleSerial.ino
@@ -0,0 +1,20 @@
+char inputString[255] = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+unsigned int stringIndex = 0;
+
+void handleSerial() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString[stringIndex] = inChar;
+    stringIndex += 1;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      inputString[stringIndex] = '\0';//Maybe this fixes the tx buffer issue, rather implicit. Maybe better to set buffer size.
+      stringComplete = true;
+      stringIndex = 0;
+    }
+  }
+}
---
+++ handleUDP.ino
@@ -0,0 +1,37 @@
+void handleUDP() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    // receive incoming UDP packets
+    //Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    if (incomingPacket[0] == '#') {
+      remoteIP = Udp.remoteIP();
+    }
+    Serial.printf(incomingPacket);
+
+
+    // send back a reply, to the IP address and port we got the packet from
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(replyPacekt);
+    Udp.endPacket();
+  }
+
+  /*
+   * Send udp data to Arduino 
+   */
+  if (stringComplete) {
+    Udp.beginPacket(remoteIP, remoteUdpPort);
+    Udp.write(inputString);
+    Udp.endPacket();
+    stringComplete = false;
+    //Serial.println("Sent udp stuff");
+    //Serial.println(inputString);
+    
+  }
+  return;
+}
+
